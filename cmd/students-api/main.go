9b6e28f0aa549f6c175775b8fa1ba714ae97ab8e
milanr21 <milanrawal309@gmail.com>
2024-10-01 07:42:21 +0545

    running in port

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,9 +1,41 @@
 // entry point of the application
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/codersgyan/students-api/internal/config"
+)
 
 func main() {
-	fmt.Println("Welcome to student API")
+
+	// load configuration
+
+	cfg := config.MustLoad()
+
+	//database  setup
+	// setup router
+
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+
+	// setup server
+
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+
+	fmt.Printf("Welcome to student API %s", cfg.HTTPServer.Addr)
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatalf("Failed to start server")
+	}
 
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -9,13 +9,13 @@ import (
 )
 
 type HTTPServer struct {
-	Addr string
+	Addr string `yaml: "address" env-required: "true"`
 }
 
 type Config struct {
 	Env         string `yaml: "env" env: "ENV" env-required: "true"`
 	StoragePath string `yaml: "storage-path" env-required: "true"`
-	HTTPServer  `yaml: "http-server"`
+	HTTPServer
 }
 
 func MustLoad() *Config {
